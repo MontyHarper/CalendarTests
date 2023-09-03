10a32e81bc90b6674e1a6281ed0e60f5883c8758
Monty Harper <132530279+MontyHarper@users.noreply.github.com>
2023-09-02 19:11:18 -0500

    Initial Commit

---
+++ CalendarTests/CalendarTestsApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalendarTestsApp.swift
+//  CalendarTests
+//
+//  Created by Monty Harper on 9/2/23.
+//
+
+import SwiftUI
+
+@main
+struct CalendarTestsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
